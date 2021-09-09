f9cc2705a15a573da2a13288b5488d99aaeca646
Your Name <zaffrina@mail.ru>
2021-09-09 18:01:37 +0800

    09.09

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,6 +1,6 @@
 
 //Просим данные у пользователя + обявляем переменные и объект
-let money = prompt("Ваш бюджет на месяц?")
+let money = +prompt("Ваш бюджет на месяц?")
     time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
@@ -9,24 +9,46 @@ let appData = {
     expenses :  {},
     optionalExpenses : {},
     income : [],
-    savings: false 
+    savings: false,
+    moneyPerDay
 };
 
 for (let i = 0; i < 2; i++)
 {
     let userIn = prompt("Введите обязательную статью расходов в этом месяце")
-    userIn2 = prompt("Во сколько обойдется?");
+    let userIn2 = prompt("Во сколько обойдется?");
 
-    if ( typeof(userIn) === 'string' && (typeof(userIn)) != null && (typeof(b)) != null  ){
+    if ( typeof(userIn) === 'string' && (typeof(userIn)) != null && (typeof(userIn2)) != null
+    && userIn != ''&& userIn2 != '' && userIn.length < 50  )
+    {
+        appData.expenses[userIn] = userIn2;
+    }
+    else {
 
     }
-    appData.expenses[userIn] = userIn2;
+    
+}
+
+// Выводим бюджет
+appData.moneyPerDay = appData.budjet/30;
+ alert("Ваш бюджет на день " + appData.moneyPerDay);
+
+ if (appData.moneyPerDay < 100) {
+console.log("Минимальный уровень достатка");
+}
+else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
+    console.log("Средний уровень достатка");
+}
+else if (appData.moneyPerDay < 2000 ){
+    console.log("Высокий уровень достатка");
+} 
+else {
+    console.log("Ошибка");
 }
 
 
-// Выводим бюджет
+ 
 
- alert("Ваш бюджет на день");
- alert(appData.budjet/30);
+ 
 
 
