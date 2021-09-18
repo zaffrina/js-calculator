01718168de1df0af67caba9aaa0ca1ebacad1768
Your Name <zaffrina@mail.ru>
2021-09-18 12:28:32 +0800

    18/09

---
+++ HomeWork/1/Untitled-1.js
@@ -0,0 +1,22 @@
+//Просим данные у пользователя + обявляем переменные и объект
+let money = prompt("Ваш бюджет на месяц?")
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+    budjet : money,
+    timeData : time,
+    expenses :  {},
+    optionalExpenses : {},
+    income : [],
+    savings: false 
+}
+
+let userIn = prompt("Введите обязательную статью расходов в этом месяце")
+    userIn2 = prompt("Во сколько обойдется?")
+    userIn1 = prompt("Введите обязательную статью расходов в этом месяце")
+    userIn3 = prompt("Во сколько обойдется?");
+// Записываем данные и выводим бюджет
+ appData.expenses.userIn = userIn2;
+ appData.expenses.userIn1 = userIn3;
+ alert("Ваш бюджет на день");
+ alert(appData.budjet/30);
+
